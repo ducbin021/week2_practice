d83e5ad211be4e0ac0e21571b2910deab444313b
duc <nguyenhuungocduc>
2019-08-23 17:12:33 +1000

    Initial Commit

---
+++ week2_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week2_practice
+//
+//  Created by Nguyen Huu Ngoc Duc on 23/8/19.
+//  Copyright © 2019 Nguyen Huu Ngoc Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week2_practiceTests/week2_practiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  week2_practiceTests.swift
+//  week2_practiceTests
+//
+//  Created by Nguyen Huu Ngoc Duc on 23/8/19.
+//  Copyright © 2019 Nguyen Huu Ngoc Duc. All rights reserved.
+//
+
+import XCTest
+@testable import week2_practice
+
+class week2_practiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
